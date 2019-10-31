d282fd793c7bbdb201d1e580d9e4f04d911c251c
Ahmed Abdelzim <toshiba20.ahmed@gmail.com>
2019-10-31 12:49:40 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p 'Enter Your Linode Token: ' TOKEN
+read -p 'Enter Number of proxies: ' PROXNUM
+sudo apt-get -y update
+#sudo apt-get -y upgrade
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get -y update
+sudo apt-get -y install apache2 libapache2-mod-php7.2 php7.2 php7.2-xml php7.2-gd php7.2-opcache php7.2-mbstring php7.2-curl memcached php-memcached
+sudo service apache2 restart
+sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
+sudo chmod a+x /usr/local/bin/youtube-dl
+hash -r
+sudo chmod a+x /usr/local/bin/youtube-dl
+rm /var/www/html/*
+git clone https://github.com/majedoh/lin_y /var/www/html
+mv /var/www/html/config-sample.php /var/www/html/config.php
+sed -i "s/\"Your Linode access token\"/\"$TOKEN\"/g" /var/www/html/config.php
+sed -i "s/3/$PROXNUM/g" /var/www/html/config.php
+sudo service apache2 restart
+php /var/www/html/create.php
+echo "Please allow 5 minutes before you start using site"
