d009d3f7fd443abaac418980179fb1f0e8eb18bc
Ahmed Abdelazim <toshiba20.ahmed@gmail.com>
2019-11-07 04:47:05 -0500

    modified index to avoid undefined var error

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-include 'functions.php';
+
 include 'config.php';
 if ($_REQUEST['t'] !== $ACCESS_T){
   die("Authentication error");
   }
+include 'functions.php';
 header('Content-Type: application/json');
 $proxies = new Memcached();
 $proxies->addServer("127.0.0.1", 11211);
